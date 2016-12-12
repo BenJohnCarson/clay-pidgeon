2017147fe19a49dd8b561c8dd3dfa391bbd1489b
Benjamin Carson <benjohncarson@gmail.com>
2016-12-12 16:14:14 +0000

    Passes all tests

---
+++ clay-pidgeon.rb
@@ -0,0 +1,19 @@
+def shoot(results_array)
+    p1_score, p2_score = 0, 0
+
+    results_array.each do |results|
+        if results[1]
+            multi = 2
+        else
+            multi = 1
+        end
+
+        results[0][:P1].scan(/X+/) { |x| p1_score += (x.length * multi) }
+        results[0][:P2].scan(/X+/) { |x| p2_score += (x.length * multi) }
+    end
+    return "Draw!" if p1_score == p2_score
+    p1_score > p2_score ? "Pete Wins!" : "Phil Wins!"
+end
+
+results_array = [[{"P1":"XX", "P2":"XO"}, true], [{"P1":"OX", "P2":"OO"}, false], [{"P1":"XX", "P2":"OX"}, true]]
+print shoot(results_array)
